42d28ab9c7fea05ea3dd30aae38e86045252645c
Alexander Shkurlatov <shkurlatov80@gmail.com>
2023-04-15 14:24:25 +1200

    Add Camera follow system

--- Assets/LittleHunter/Sources/MonoBehaviours/SandboxSystem.cs
+++ Assets/LittleHunter/Sources/MonoBehaviours/SandboxSystem.cs
@@ -8,23 +8,28 @@ namespace LittleHunter
     public class SandboxSystem : MonoBehaviourPunCallbacks
     {
         [SerializeField] private List<Transform> spawnPoints;
+        [SerializeField] private Transform cameraRoot;
+        [SerializeField] private Transform cameraPivot;
 
         private Systems _systems;
         private Contexts _contexts;
 
         private Systems _physicSystems;
+        private Systems _lateFixedUpdateSystems;
 
         private void Awake()
         {
             _contexts = Contexts.sharedInstance;
             _systems = new Feature("Game System");
             _physicSystems = new Feature("Physics Systems");
+            _lateFixedUpdateSystems = new Feature("Late Systems");
 
             _systems.Add(new CreateSpawnPointSystem(_contexts, spawnPoints));
             _systems.Add(new CreateLocalPlayerSystem(_contexts));
 
             _physicSystems.Add(new MovePlayerSystem(_contexts));
             _physicSystems.Add(new RotatePlayerSystem(_contexts));
+            _lateFixedUpdateSystems.Add(new CameraFollowTargetSystem(_contexts, cameraRoot));
         }
 
         private void Start()
@@ -32,6 +37,7 @@ namespace LittleHunter
             _contexts.network.isPendingConnection = false;
             _systems.Initialize();
             _physicSystems.Initialize();
+            _lateFixedUpdateSystems.Initialize();
         }
 
         private void Update()
@@ -43,7 +49,9 @@ namespace LittleHunter
         private void FixedUpdate()
         {
             _physicSystems.Execute();
+            _lateFixedUpdateSystems.Execute();
             _physicSystems.Cleanup();
+            _lateFixedUpdateSystems.Cleanup();
         }
     }
 }
